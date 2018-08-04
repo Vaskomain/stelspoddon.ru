841e29f39db0487fad51db23fcaa079823954091
Vasiliy Konovalov <vasko.main@gmail.com>
2018-08-04 20:55:24 +0300

    evropoddony page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 ;
-LoadProducts = (element) => {
-    axios.get('poddony.json')
+LoadProducts = (host,element) => {
+    axios.get(host+'/poddony.json')
         .then((response) => {
             //Вывод категорий
             let categoryPills = 
@@ -18,32 +18,43 @@ LoadProducts = (element) => {
             let productDeck = 
             `<div class="tab-content" id="products-pills-content">
                 <div class="tab-pane fade show active" id="products-hits" role="tabpanel" aria-labelledby="products-hits-tab">`
-                + productCardDeck(response.data.products.filter(productData => productData.hit))
+                + productCardDeck(response.data.products.filter(productData => productData.hit),false,host)
                 + `</div>` 
             //here will be categorized tabs
             + response.data.categories.map((categoryData,index) => {
                 return `<div class="tab-pane fade" id="products-${categoryData.id}" role="tabpanel" aria-labelledby="products-${categoryData.id}-tab">`
-                        + productCardDeck(response.data.products.filter(productData => productData.category === categoryData.id))
+                        + productCardDeck(response.data.products.filter(productData => productData.category === categoryData.id),false,host)
                         +`</div>`
             });
             element.innerHTML = categoryPills + productDeck;
         });
 };
 
-productCardDeck = (products) => {
+LoadSetOfProducts = (host,element,categoryId) => {
+    axios.get(host+'/poddony.json')
+        .then((response) => {
+            //Вывод самих продуктов
+            element.innerHTML = productCardDeck(response.data.products.filter(productData => productData.category === categoryId),true,host);
+        });
+};
+
+
+productCardDeck = (products,singlePack,host) => {
+    let hostname = (host ? host : '');
     let deck = ``;
-    let columns = 3;
-    if (!(products.length %  3 == 0) && (products.length % 4 == 0)) {
-        columns = 4;
-    };
+    let columns = 4;
+    //let columns = 3;
+    //if (!(products.length %  3 == 0) && (products.length % 4 == 0)) {
+    //    columns = 4;
+    //};
     let rows = Math.ceil(products.length / columns);
      for (let currentRow = 1; currentRow <= rows; currentRow++) {
          deck += `<div class="card-deck palletes-cards pt-1 mt-0">`
          for (let currentColumn = 1; currentColumn <= columns; currentColumn ++) {
             index = (currentRow - 1) * columns + (currentColumn - 1);
             if (index < products.length) {
-                deck += productCardElement(products[index]);
-            }  else
+                deck += productCardElement(products[index],hostname);
+            }  else if (!singlePack)
             {
                 deck += `<div class="card invisible"></div>`;
             };
@@ -53,11 +64,13 @@ productCardDeck = (products) => {
      return deck;
 };
 
-productCardElement = (productData) => {
+productCardElement = (productData,host) => {
+    let hostname = (host ? host + "/" : '');
+    console.log(hostname);
     return `                
     <div class="card">`
-    + (productData.link ? `<a href="${productData.link}"> ` : ``)
-        + `<img class="card-img-top" src="${productData.imageFile}" alt="${productData.name}">`
+    + (productData.link ? `<a href="${hostname}${productData.link}"> ` : ``)
+        + `<img class="card-img-top" src="${hostname}${productData.imageFile}" alt="${productData.name}">`
     + (productData.link ? ` </a>` : ``)
     +`<div class="card-body">
         <h5 class="card-title text-center">${productData.name}</h5>
